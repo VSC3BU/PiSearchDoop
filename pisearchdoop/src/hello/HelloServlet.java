546f44cbf9826a95ecb0f2f76b899f7230012c4b
gen <gen@GEN_PC>
2014-04-19 21:10:41 +0900

    HelloWorldをコミット

---
+++ pisearchdoop/src/hello/HelloServlet.java
@@ -0,0 +1,28 @@
+package hello;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public HelloServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    request.setAttribute("HELLO_DATA", "HELLO!! SERVLET to JSP!!");
+	    request.getRequestDispatcher("HelloJSP.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
