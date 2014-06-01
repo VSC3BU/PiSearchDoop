c785fb531c98f03e31582a1c0814e727662796ff
gen hirano <genhirano@hotmail.com>
2014-06-01 14:00:09 +0900

    消すコミット

--- src/hello/HelloServlet.java
+++ src/hello/HelloServlet.java
@@ -18,6 +18,36 @@ public class HelloServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    request.setAttribute("HELLO_DATA", "HELLO!! SERVLET to JSP!!");
+	    request.getRequestDispatcher("HelloJSP.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doGet(request, response);
+	}
+
+}
+=======
+<<<<<<< HEAD
+package hello;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public HelloServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		request.setAttribute("DATA", "HELLO!");
 		request.getRequestDispatcher("HelloJSP.jsp").forward(request, response);
@@ -55,3 +85,4 @@ public class HelloServlet extends HttpServlet {
 
 }
 >>>>>>> 53ed96e6af51ec2dd6de8f9446cb1b56f57d10bb
+>>>>>>> origin/develop
